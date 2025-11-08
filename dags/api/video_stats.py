de7e381b1585a4673dc85e79788629b91aff11ee
Ashutosh8897 <ashutosh.dubey4@tcs.com>
2025-11-08 10:15:00 +0530

    committing soda test files

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,18 +1,26 @@
 import requests
 import json
-import os
-from dotenv import load_dotenv
 from datetime import date
 
-load_dotenv(dotenv_path = "./.env")
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path = "./.env")
 
-api_key = os.getenv("API_KEY")
-channel_name = "MrBeast"
+
+from airflow.decorators import task
+from airflow.models import Variable
+
+
+
+
+api_key = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 max_results = 50
 
+@task
 def get_playlistid():
     try:
-        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_name}&key={api_key}"
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={api_key}"
 
         response =requests.get(url)
         response.raise_for_status() 
@@ -27,7 +35,7 @@ def get_playlistid():
     except requests.exceptions.RequestException as e:
         raise e 
     
-
+@task
 def get_videoID(playlistID):
     video_IDs = []
     pageToken = None
@@ -58,7 +66,7 @@ def get_videoID(playlistID):
     
 
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -91,7 +99,7 @@ def extract_video_data(video_ids):
                     "duration" : contentDetails['duration'],
                     "viewCount" : statistics.get('viewCount', None),
                     "likeCount" : statistics.get('likeCount',None),
-                    "commnetCount" : statistics.get('commnetCount', None)
+                    "commentCount" : statistics.get('commentCount', None)
                 }
 
                 extracted_data.append(video_data)
@@ -100,6 +108,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json" 
     with open(file_path, "w", encoding= "utf-8") as json_outputfile:
---
+++ dags/main.py
@@ -0,0 +1,64 @@
+from airflow import DAG
+import pendulum
+from datetime import datetime, timedelta
+from api.video_stats import get_playlistid, get_videoID, extract_video_data, save_to_json
+
+from datawarehouse.dwh import staging_table, core_table
+
+#define the Local time
+local_tz = pendulum.timezone("Asia/Kolkata")
+
+
+#Default args
+default_args = {
+    "owner" : "Ashutosh",
+    "depands_on_past" : False,
+    "email_on_failure" : False,
+    "email_on_retry" : False,
+    "email" : "ashutoshd813@gmail.com",
+    # "retries" : 1,
+    # "retry_delay" : timedelta(minutes = 5),
+    "max_active_run" : 1,
+    "dagrun_timeout" : timedelta(hours=1),
+    "start_date" : datetime(2025,1,1, tzinfo = local_tz)
+    # "end_date" : datetime(2030,12,31, tzinfo= local_tz)
+}
+
+with DAG(
+    dag_id = "produce_json",
+    default_args = default_args,
+    description = "DAG to produce JSON file with raw data.",
+    schedule = '0 14 * * *',
+    catchup = False
+) as dag:
+    
+
+    #define tasks
+    playlist_id = get_playlistid()
+    video_ids = get_videoID(playlist_id)
+    extract_data = extract_video_data(video_ids)
+    save_to_json_task = save_to_json(extract_data)
+
+
+    #Define dependencies
+    playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    # Define dependencies
+    update_staging >> update_core
+
+
+ 
